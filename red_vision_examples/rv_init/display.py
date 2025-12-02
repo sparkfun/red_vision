cad8d17f9d1533594d4e3cec96e987c625f23f3d
Dryw Wade <dryw.wade@sparkfun.com>
2025-12-01 17:29:16 -0700

    Add DVI HSTX to rv_init display example

--- red_vision_examples/rv_init/display.py
+++ red_vision_examples/rv_init/display.py
@@ -35,3 +35,32 @@ display = st7789_spi.ST7789_SPI(
 #     pin_cs = 17,
 #     rotation = 1
 # )
+
+################################################################################
+# DVI
+################################################################################
+
+# HSTX interface. This is only available on Raspberry Pi RP2350 processors.
+# Create the singleston DVI_HSTX display instance.
+# display = dvi_rp2_hstx.DVI_HSTX(
+#     width = 320,
+#     height = 240,
+
+#     # 4 different color modes are supported, though not all color modes can be
+#     # used with all resolutions due to memory constraints.
+#     # color_mode = dvi_rp2_hstx.DVI_HSTX.COLOR_BGR233,
+#     # color_mode = dvi_rp2_hstx.DVI_HSTX.COLOR_GRAY8,
+#     color_mode = dvi_rp2_hstx.DVI_HSTX.COLOR_BGR565,
+#     # color_mode = dvi_rp2_hstx.DVI_HSTX.COLOR_BGRA8888,
+
+#     # Pins default to the SparkFun HSTX to DVI Breakout:
+#     # https://www.sparkfun.com/sparkfun-hstx-to-dvi-breakout.html
+#     # pin_clk_p = 14,
+#     # pin_clk_n = 15,
+#     # pin_d0_p  = 18,
+#     # pin_d0_n  = 19,
+#     # pin_d1_p  = 16,
+#     # pin_d1_n  = 17,
+#     # pin_d2_p  = 12,
+#     # pin_d2_n  = 13
+# )
